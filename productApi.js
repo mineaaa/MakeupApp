4791fb87f78583e2dc0934e86a33ee1f95930867
Minea <117983209+mineaaa@users.noreply.github.com>
2024-11-05 15:24:58 +0200

    komponentti listaamiselle ja apia varten tiedosto update

---
+++ productApi.js
@@ -0,0 +1,8 @@
+
+export default function FetchProducts(productType) {
+    return fetch(`${process.env.EXPO_PUBLIC_API_URL}?product_type=${productType}`)
+        .then(response => {
+            if (!response.ok) throw new Error('Something went wrong: ' + response.statusText);
+            return response.json();
+        });
+}
