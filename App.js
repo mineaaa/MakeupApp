e35855397b8bf3f79b5c8630b3437c9e22323326
Minea <117983209+mineaaa@users.noreply.github.com>
2024-11-07 12:13:59 +0200

    tab navigator tuotu sovellukseen

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './Home';
-import { getDatabase } from 'firebase/database';
-import Search from './Search';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Home';
+import Search from './Search';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
-      <Search />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home';
+              } else if (route.name === 'Search') {
+                iconName = 'search';
+              }
+
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'pink',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Search" component={Search} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
-
-
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
